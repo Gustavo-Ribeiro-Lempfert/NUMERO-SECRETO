34ce54cc38163f795d39df145205d4b7c66957de
Gustavo Ribeiro Lempfert <gustavoribe00@outlook.com>
2023-03-21 14:44:53 -0300

    Aplicando reconhecimento de voz com web speech api

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,20 @@
+const elementoChute = document.getElementById('chute')
+
+const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-br'
+recognition.start();
+
+recognition.addEventListener('result',onSpeak)
+
+function onSpeak(e){
+    chute = e.results[0][0].transcript
+    exibehuteNaTela(chute)
+    verificaChute(chute)
+}
+
+function exibehuteNaTela(chute){
+    elementoChute.innerHTML = `<div>Vocêdisse:</div>
+                     <span class="box">${chute}</span>`
+}
